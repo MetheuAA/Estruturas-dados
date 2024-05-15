13705e13e71542faee1458543a70509481e4c6b8
MetheuAA <matheo200817@gmail.com>
2024-05-15 08:48:29 -0400

    add values

---
+++ mapas/values.js
@@ -0,0 +1,11 @@
+let mapa = new Map();
+mapa.set('chave1', 'valor1');
+mapa.set('chave2', 'valor2');
+
+let iterador = mapa.values();
+for (let valor of iterador) {
+    console.log(valor);
+}
+// Saída:
+// valor1
+// valor2

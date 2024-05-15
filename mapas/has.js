692082f5e55d0eef5f4c37246b36a7e414f913ba
MetheuAA <matheo200817@gmail.com>
2024-05-15 08:45:59 -0400

    add set

---
+++ mapas/has.js
@@ -0,0 +1,5 @@
+let mapa = new Map();
+mapa.set('chave1', 'valor1');
+
+console.log(mapa.has('chave1')); // Saída: true
+console.log(mapa.has('chave2')); // Saída: false
---
+++ mapas/keys.js
@@ -0,0 +1,10 @@
+let mapa = new Map();
+mapa.set('chave1', 'valor1');
+mapa.set('chave2', 'valor2');
+
+let iterador = mapa.keys();
+for (let chave of iterador) {
+    console.log(chave);
+}// Saída:
+// chave1
+// chave2
